eb7ddb0f303dd07a1a27f4eaa0af3ea07bdd4f26
Chhavi <150151645+g-chhavii@users.noreply.github.com>
2024-04-16 23:46:11 +0530

    Update app.js

--- quote-generator/app.js
+++ quote-generator/app.js
@@ -29,19 +29,4 @@ function generate(){
 
 }
 
-// //PROJECT 2
-// document.querySelector("#message-input").addEventListener("keydown", (event) => {
-//     if(event.key === "Enter")
-//       getMessage();
-//   });
-  
-//   document.querySelector("#submit").addEventListener("click", () => {
-//       getMessage();
-//   });
-  
-//   getMessage = () => {
-//     document.querySelector("#message-output").textContent = document.querySelector("#message-input").value;
-//     document.querySelector("#message-input").value = "";
-//   }
-
-
+ 
